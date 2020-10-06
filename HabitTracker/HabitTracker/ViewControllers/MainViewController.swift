ca6b1062e4df9d81a4a35494888ca45b1876eff4
Oliwia Michalak <oliwia.michalak@gmail.com>
2020-10-06 19:31:15 +0100

    feat: adding UI components
    added:
    - label
    - table view

--- HabitTracker/HabitTracker/ViewControllers/MainViewController.swift
+++ HabitTracker/HabitTracker/ViewControllers/MainViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 class MainViewController: UIViewController {
     
     // MARK: Private properites
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont(name: "Roboto", size: 25)
+        label.numberOfLines = 0
+        label.text = "You can do it!"
+        label.textColor = .systemPurple
+        return label
+    }()
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemGray
+        return tableView
+    }()
 
     // MARK: Lifecycle
     override func viewDidLoad() {
@@ -19,7 +34,31 @@ class MainViewController: UIViewController {
     
     // MARK: Private functions
     private func setupUI() {
+        view.backgroundColor = .systemBackground
         
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+                titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                titleLabel.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1),
+                titleLabel.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8)
+            ]
+        )
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                tableView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.8),
+                tableView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.7),
+                tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20)
+            ]
+        )
     }
 
 }
